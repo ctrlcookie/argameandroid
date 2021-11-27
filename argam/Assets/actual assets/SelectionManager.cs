563cebf51e6cc1dc376f831c0b265db5db9db086
ctrlcookie <regardingquiet@gmail.com>
2021-11-27 16:53:05 +0000

    raycasting, buttons, etc

---
+++ argam/Assets/actual assets/SelectionManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] public Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private string selectableTag = "Selectable";
+
+    private Transform _selection;
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+
+            if (selection.CompareTag(selectableTag))
+            {
+              var selectionRenderer = selection.GetComponent<Renderer>();
+
+              if (selectionRenderer != null)
+              { 
+                selectionRenderer.material = highlightMaterial;
+              }
+
+                _selection = selection;
+            }
+        }
+    }
+}
